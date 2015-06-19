cf5bceda84271dac23c5b83048d477bb0edaa023
housseindhayne <dhayne_houssein@hotmail.com>
2015-06-19 16:54:51 -0400

    replace absolute url by relative one for json booksData file

--- public/js/services/books.js
+++ public/js/services/books.js
@@ -1,5 +1,5 @@
 app.factory('books', ['$http', function($http) {
-  return $http.get('http://localhost:8456/booksData.json')
+  return $http.get('/booksData.json')
          .success(function(data) {
            return data;
          })
