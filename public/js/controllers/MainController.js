0405364d5cbeca7d0f8e121265701e26b977d273
Houssein <dhayne_houssein@hotmail.com>
2015-06-21 19:42:35 -0400

    misc

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -9,4 +9,11 @@ app.controller('MainController',['$scope','books',function($scope,books){
    $scope.minusOne = function(index) {
   $scope.products[index].dislikes += 1;
 };
+$scope.reserve = function(index) {
+if($scope.products[index].reserve=='reserve'){
+  $scope.products[index].reserve = 'release'
+}else {
+  $scope.products[index].reserve = 'reserve'
+}
+}
 }]);
